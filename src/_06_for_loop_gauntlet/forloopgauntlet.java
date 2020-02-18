545b099e4963eb1a64fac7da4b770a505f8c024e
315815 <315815@0338S-Z-90TDE0N.sdcs.local>
2020-02-18 14:21:44 -0800

    awdadadawd

--- src/_06_for_loop_gauntlet/forloopgauntlet.java
+++ src/_06_for_loop_gauntlet/forloopgauntlet.java
@@ -11,9 +11,95 @@ public class forloopgauntlet {
 	for (int c = 0; c < 101; c++) {
 		
 	}
+	for (int d = 2; d <= 100; d+= 2) {
+		System.out.println(d);
+	}
+	for (int e = 1; e <= 99; e+=2) {
+		
+	}
+	for (int f = 1; f <= 500; f++) {
+		if(f%2==0) { 
+		System.out.println(f + " is even");
+		}
+		else { 
+		System.out.println(f + " is odd");
+		}
+	}
+	for (int g = 7; g <= 777; g++) {
+		if(g%7==0) {
+		System.out.println(g);
+		}
+	}
+	for (int h = 2005; h <= 2020; h++) {
+		if(h==2005) {
+			System.out.println("In 2005, I was born.");
+		}
+		if(h==2006) {
+			System.out.println("In 2006, I was 1 year old.");
+		}
+		if(h==2007) {
+			System.out.println("In 2007, I was 2 years old.");
+		}
+		if(h==2008) {
+			System.out.println("In 2008, I was 3 years old.");
+		}
+		if(h==2009) {
+			System.out.println("In 2009, I was 4 years old.");
+		}
+		if(h==2010) {
+			System.out.println("In 2010, I was 5 years old.");
+		}
+		if(h==2011) {
+			System.out.println("In 2011, I was 6 years old.");
+		}
+		if(h==2012) {
+			System.out.println("In 2012, I was 7 years old.");
+		}
+		if(h==2013) {
+			System.out.println("In 2013, I was 8 years old.");
+		}
+		if(h==2014) {
+			System.out.println("In 2014, I was 9 years old.");
+		}
+		if(h==2015) {
+			System.out.println("In 2015, I was 10 years old.");
+		}
+		if(h==2016) {
+			System.out.println("In 2016, I was 11 years old.");
+		}
+		if(h==2017) {
+			System.out.println("In 2017, I was 12 years old.");
+		}
+		if(h==2018) {
+			System.out.println("In 2018, I was 13 years old.");
+		}
+		if(h==2019) {
+			System.out.println("In 2019, I was 14 years old.");
+		}
+		if(h==2020) {
+			System.out.println("In 2020, I was 15 years old.");
+		}
+for (int k = 0; k < 3; k++) {
 	
+
+		for (int j = 1; j < 4; j++) {
+			System.out.print(j+(k*3));
+		}
+		System.out.println();
+}	
+for (int l = 0; l < 10; l++) {
 	
-	
+
+	for (int i = 1; i < 11; i++) {
+	System.out.print(i+(l*10));
+}
+
+		System.out.println();
+}
+		
+		
+		
+	}
 	
 	
 	
